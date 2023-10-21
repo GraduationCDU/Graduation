a9af6f2f7a50be0e8506ac1a5aed773ec80296a4
onelibero <1792208603@qq.com>
2023-10-21 18:33:39 +0800

    feat: add readme

--- back_end/src/main/java/cdu/gu/back_end/BackEndApplication.java
+++ back_end/src/main/java/cdu/gu/back_end/BackEndApplication.java
@@ -1,4 +1,4 @@
-package com.example.back_end;
+package cdu.gu.back_end;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Graduation
+
+## 简介
+
+该仓库是准备存放后端项目的，后期会优化目录。
+
+## 技术栈
+
+后端使用`SpringBoot`框架作为主体，数据库采用`MySQL`、`Redis`，数据交互采用`mybatis-plus`实现更加方便。
