35cf55655c8c890250edeb94b43c998e17336fda
onelibero <1792208603@qq.com>
2023-11-04 21:19:22 +0800

    新增mapper

--- back_end/back-common/src/main/java/cdu/gu/common/mapper/AdminUserRoleMapper.java
+++ back_end/back-common/src/main/java/cdu/gu/common/mapper/AdminUserRoleMapper.java
@@ -1,4 +1,9 @@
 package cdu.gu.common.mapper;
 
-public interface AdminUserRoleMapper {
+import cdu.gu.common.pojo.AdminUserRole;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AdminUserRoleMapper extends BaseMapper<AdminUserRole> {
 }
